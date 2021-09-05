fbbd2823c45c7ad1272983ca56bd15d0aebdf0d2
furuichi-kotaro <happy1998k@gmail.com>
2021-09-05 18:06:52 +0900

    generate graphql files

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,21 @@
+module Types
+  class QueryType < Types::BaseObject
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
+
+    field :users, [Types::UserType], null: false
+    def users
+      User.all
+    end
+
+    field :user, Types::UserType, null: false do
+      argument :id, Int, required: false
+    end
+    def user(id:)
+      User.find(id)
+    end
+  end
+end
